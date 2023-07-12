6c8704e74b7a3699cc1d38932b01a2274d65a5be
praveen.kumar <praveenkumarnew2019@gmail.com>
2023-07-12 13:39:54 +0530

    formadded

---
+++ src/main/java/com/mycompany/echo/AllTasksAndServices/GrantResourceForm.java
@@ -0,0 +1,78 @@
+package com.mycompany.echo.AllTasksAndServices;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.microsoft.bot.builder.MessageFactory;
+import com.microsoft.bot.builder.TurnContext;
+import com.microsoft.bot.schema.Activity;
+import com.microsoft.bot.schema.Attachment;
+import org.springframework.stereotype.Component;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Component
+public class GrantResourceForm {
+    private String getAdaptiveCardJson() {
+        String adaptiveCardJson=  "{\n" +
+                "  \"type\": \"AdaptiveCard\",\n" +
+                "  \"body\": [\n" +
+                "    {\n" +
+                "      \"type\": \"TextBlock\",\n" +
+                "      \"text\": \"Grant Resource\",\n" +
+                "      \"weight\": \"Bolder\",\n" +
+                "      \"size\": \"Medium\"\n" +
+                "    },\n" +
+                "    {\n" +
+                "      \"type\": \"Input.Text\",\n" +
+                "      \"id\": \"email\",\n" +
+                "      \"placeholder\": \"USER_EMAIL\"\n" +
+                "    },\n" +
+                "    {\n" +
+                "      \"type\": \"Input.Text\",\n" +
+                "      \"id\": \"resource\",\n" +
+                "      \"placeholder\": \"RESOURCE_NAME\"\n" +
+                "    },\n" +
+                "    {\n" +
+                "      \"type\": \"Input.Text\",\n" +
+                "      \"id\": \"hour\",\n" +
+                "      \"placeholder\": \"TIME_IN_HOUR\"\n" +
+                "    },\n" +
+                "    {\n" +
+                "      \"type\": \"Input.Text\",\n" +
+                "      \"id\": \"minute\",\n" +
+                "      \"placeholder\": \"TIME_IN_MINUTE\"\n" +
+                "    }\n" +
+                "  ],\n" +
+                "  \"actions\": [\n" +
+                "    {\n" +
+                "      \"type\": \"Action.Submit\",\n" +
+                "      \"title\": \"Grant\"\n" +
+                "    }\n" +
+                "  ],\n" +
+                "  \"$schema\": \"http://adaptivecards.io/schemas/adaptive-card.json\",\n" +
+                "  \"version\": \"1.5\"\n" +
+                "}";
+
+        return adaptiveCardJson;
+    }
+    public void getForm(TurnContext turnContext){
+        Attachment cardAttachment = new Attachment();
+        cardAttachment.setContentType("application/vnd.microsoft.card.adaptive");
+        try {
+            cardAttachment.setContent(new ObjectMapper().readTree(getAdaptiveCardJson()));
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+
+//            cardAttachment.setContent(JsonNodeFactory.instance.textNode(getAdaptiveCardJson()));
+
+        Activity reply = MessageFactory.attachment(cardAttachment);
+        Map<String, Object> channelData = new HashMap<>();
+        channelData.put("isAdaptiveCard", true);
+        reply.setChannelData(channelData);
+
+        // Send the reply
+        turnContext.sendActivity(reply).thenApply(resourceResponse -> null);
+    }
+}
